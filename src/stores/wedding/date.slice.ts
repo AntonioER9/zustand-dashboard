f959c9f94550643593b7bcbd39ff6ef619e9eab9
Antonio Espinoza <anespinoza@baufest.com>
2024-05-31 11:23:00 -0400

    implemented store & slice for wedding

--- src/stores/wedding/date.slice.ts
+++ src/stores/wedding/date.slice.ts
@@ -25,7 +25,7 @@ export const createDateSlice: StateCreator<DateSlice> = (set, get) => ({
 
 
   eventHHMM: () => {
-    const hours = get().eventDate.getHours().toString().padStart(2, '0');
+    const hours = get().eventDate.getHours().toString().padStart(2, '0'); //01 si fueran las 1 am
     const minutes = get().eventDate.getMinutes().toString().padStart(2, '0');
 
     return `${hours}:${minutes}`;
